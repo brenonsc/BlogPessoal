312e2086761611a64bc773182b059728032afd6b
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-09-22 15:09:03 -0300

    Resolvida implementação incorreta de injeção de dependência

--- BlogPessoal/Controllers/PostagemController.cs
+++ BlogPessoal/Controllers/PostagemController.cs
@@ -1,4 +1,5 @@
 using BlogPessoal.Model;
+using BlogPessoal.Service;
 using BlogPessoal.Service.Implements;
 using FluentValidation;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -10,10 +11,10 @@ namespace BlogPessoal.Controllers;
 [ApiController]
 public class PostagemController : ControllerBase
 {
-    private readonly PostagemService _postagemService;
+    private readonly IPostagemService _postagemService;
     private readonly IValidator<Postagem> _postagemValidator;
     
-    public PostagemController(PostagemService postagemService, IValidator<Postagem> postagemValidator)
+    public PostagemController(IPostagemService postagemService, IValidator<Postagem> postagemValidator)
     {
         _postagemService = postagemService;
         _postagemValidator = postagemValidator;
